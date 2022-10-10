ca6d9694955ce1f2b683ac3049c05bec98f05cec
Rinse Kloek <rinse@solcon.nl>
2022-10-10 21:38:07 +0200

    Update programma3.py

--- programma3.py
+++ programma3.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
-# Programma 2
+# Programma 3
 
 leeftijd    = 8
 if (leeftijd < 16):
-  print(f"{leeftijd} is kleiner dan 16 ")
-  
-  
-  
+  print(f"{leeftijd} is kleiner dan 16 "
